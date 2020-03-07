d338679037c5a86ebd84e5c1823d8e17ab6971a5
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-03-07 22:47:18 +0100

    Temporarily hard-coding Vivado 2016.4 path so the scripts they usable even with a newer Vivado installed
    --HG--
    branch : issue//HAST-157

--- CreateSdkWorkspace.bat
+++ CreateSdkWorkspace.bat
@@ -2,19 +2,9 @@
 
 title Launching Xilinx SDK...
 
-rem For current user-only installations this key will exist, for system-wide ones the other one.
-reg query "HKEY_CURRENT_USER\Software\Classes\Vivado.Project.1\Shell\Open\Command" >nul
-if %errorlevel% equ 0 (
-	for /f "tokens=4 delims= " %%r in ('reg query "HKEY_CURRENT_USER\Software\Classes\Vivado.Project.1\Shell\Open\Command" /ve') do (
-		for /f "usebackq tokens=*" %%p in (`cscript /NoLogo sdkpath.vbs %%r`) do set sdkpath=%%p
-	)
-) else (
-	echo "The registry key of the Vivado shell wasn't found for the current user. Attempting for the local machine."
-	for /f "tokens=4 delims= " %%r in ('reg query "HKEY_LOCAL_MACHINE\Software\Classes\Vivado.Project.1\Shell\Open\Command" /ve') do (
-		for /f "usebackq tokens=*" %%p in (`cscript /NoLogo sdkpath.vbs %%r`) do set sdkpath=%%p
-	)
+FOR /F "tokens=* USEBACKQ" %%F IN (`cscript sdkpath.vbs C:\Xilinx\Vivado\2016.4\bin\unwrapped\win64.o\vvgl.exe`) DO (
+SET sdkpath=%%F
 )
-
 
 echo Found Xilinx SDK at: %sdkpath%
 
